bfa70c440dbfa1cf49e5964f08dd3e6ccb715772
vladik kozynin <liekesliefhebber2000@hotmail.com>
2020-06-03 14:38:05 +0200

    f

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LaboTool_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
