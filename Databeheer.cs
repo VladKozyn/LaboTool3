67e12cb98d48a86b10e891980d69e1688a158b18
vladik kozynin <liekesliefhebber2000@hotmail.com>
2020-06-05 08:26:40 +0200

    f

---
+++ Databeheer.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace LaboTool_3
+{
+    class Databeheer
+    {
+        private string connectionString;
+        public Databeheer(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        private SqlConnection GetConnection()
+        {
+            SqlConnection connection = new SqlConnection(@"Data Source =.\SQLEXPRESS; Initial Catalog = Labo; Integrated Security = True");
+            return connection;
+        }
+
+        public List<string> vraagStraatNaamGebasseerdOpGemeente(string gemeenteNaam)
+        {
+            SqlConnection connection = GetConnection();
+            List<string> lstStraten = new List<string>();
+
+            string queryString = "SELECT straatNaam " +
+                                 "FROM Straat s " +
+                                 "JOIN dbo.Gemeente g ON s.gemeenteId = g.gemeenteId " +
+                                 "WHERE gemeenteNaam = @gemeenteNaam "+
+                                 "ORDER BY s.straatNaam ASC";
+            using (SqlCommand command = connection.CreateCommand())
+            {
+                command.CommandText = queryString;
+                SqlParameter paramId = new SqlParameter();
+                paramId.ParameterName = "@gemeenteNaam";
+                paramId.DbType = System.Data.DbType.String;
+                paramId.Value = gemeenteNaam;
+
+                command.Parameters.Add(paramId);
+                connection.Open();
+
+                try
+                {
+                    SqlDataReader dataReader = command.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        string id = (string)dataReader["straatNaam"];
+                        lstStraten.Add(id);
+
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex);
+                        return null;
+                }
+                finally
+                {
+                    connection.Close();
+                }
+                
+            }
+            return lstStraten;
+        }
+        public List<int> vraagStraatIdsGebasseerdOpGemeente(string gemeenteNaam)
+        {
+            SqlConnection connection = GetConnection();
+            List<int> lstStratenIds = new List<int>();
+
+            string queryString = "SELECT s.straatId " +
+            "FROM Straat s " +
+            "JOIN dbo.Gemeente g ON s.gemeenteId = g.gemeenteId " +
+            "WHERE g.gemeenteNaam = @gemeenteNaam " +
+            "ORDER BY s.straatId ASC";
+            using (SqlCommand command = connection.CreateCommand())
+            {
+                command.CommandText = queryString;
+                SqlParameter paramId = new SqlParameter();
+                paramId.ParameterName = "@gemeenteNaam";
+                paramId.DbType = System.Data.DbType.String;
+                paramId.Value = gemeenteNaam;
+
+                command.Parameters.Add(paramId);
+                connection.Open();
+
+                try
+                {
+                    SqlDataReader dataReader = command.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        int id = (int)dataReader["straatId"];
+                        lstStratenIds.Add(id);
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    return null;
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+            }
+            return lstStratenIds;
+        }
+        public List<int> overZicht(string gemeenteNaam)
+        {
+            SqlConnection connection = GetConnection();
+            List<int> lstStratenIds = new List<int>();
+
+            string queryString = "SELECT s.straatId " +
+            "FROM Straat s " +
+            "JOIN dbo.Gemeente g ON s.gemeenteId = g.gemeenteId " +
+            "WHERE g.gemeenteNaam = @gemeenteNaam " +
+            "ORDER BY s.straatId ASC";
+            using (SqlCommand command = connection.CreateCommand())
+            {
+                command.CommandText = queryString;
+                SqlParameter paramId = new SqlParameter();
+                paramId.ParameterName = "@gemeenteNaam";
+                paramId.DbType = System.Data.DbType.String;
+                paramId.Value = gemeenteNaam;
+
+                command.Parameters.Add(paramId);
+                connection.Open();
+
+                try
+                {
+                    SqlDataReader dataReader = command.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        int id = (int)dataReader["straatId"];
+                        lstStratenIds.Add(id);
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    return null;
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+            }
+            return lstStratenIds;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LaboTool_3
 {
@@ -6,7 +7,94 @@ namespace LaboTool_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Databeheer databeheer = new Databeheer(@"Data Source =.\SQLEXPRESS; Initial Catalog = Labo; Integrated Security = True");
+
+            //Als gebruiker wil ik een lijst van straatIDs kunnen opvragen voor een opgegeven gemeentenaam.
+            #region
+            /*
+            string gemeenteNaam = "";
+            Console.WriteLine("schijf aub één gemeentenaam");
+            gemeenteNaam = Console.ReadLine();
+            List<int> lstStraten = databeheer.vraagStraatIdsGebasseerdOpGemeente(gemeenteNaam);
+            if (lstStraten == null)
+            {
+                Console.WriteLine("Ongeldige gemeente naam");
+            }
+            else
+            {
+                foreach (int straten in lstStraten)
+                {
+                    Console.WriteLine(straten + "\n");
+                }
+            }
+            */
+            #endregion
+            //Als gebruiker wil ik een straat kunnen opvragen op basis van een meegegeven straatID.
+            //todo
+            #region
+            /*
+            string gemeenteNaam = "";
+            Console.WriteLine("schijf aub één gemeentenaam");
+            gemeenteNaam = Console.ReadLine();
+           List<string> lstStraten = databeheer.vraagStraatIdsVanGemeenteNaam(gemeenteNaam);
+            if(lstStraten == null)
+            {
+                Console.WriteLine("Ongeldige gemeente naam");
+            }
+            else
+            {
+                foreach(string straten in lstStraten)
+                {
+                    Console.WriteLine(straten + "\n");
+                }
+            }
+            */
+            #endregion
+
+            //Als gebruiker wil ik een straat kunnen opvragen op basis van de straatnaam en de gemeentenaam.
+            //todo
+            #region
+            /*
+            string gemeenteNaam = "";
+            Console.WriteLine("schijf aub één gemeentenaam");
+            gemeenteNaam = Console.ReadLine();
+           List<string> lstStraten = databeheer.vraagStraatIdsVanGemeenteNaam(gemeenteNaam);
+            if(lstStraten == null)
+            {
+                Console.WriteLine("Ongeldige gemeente naam");
+            }
+            else
+            {
+                foreach(string straten in lstStraten)
+                {
+                    Console.WriteLine(straten + "\n");
+                }
+            }
+            */
+            #endregion
+
+            //Als gebuiker wil ik alle straatnamen van een gemeente kunnen opvragen(alfabetisch gesorteerd).
+            #region
+            /*
+            string gemeenteNaam = "";
+            Console.WriteLine("schijf aub één gemeentenaam");
+            gemeenteNaam = Console.ReadLine();
+            List<string> lstStraten = databeheer.vraagStraatNaamGebasseerdOpGemeente(gemeenteNaam);
+            if (lstStraten == null)
+            {
+                Console.WriteLine("Ongeldige gemeente naam");
+            }
+            else
+            {
+                foreach (string straten in lstStraten)
+                {
+                    Console.WriteLine(straten + "\n");
+                }
+            }*/
+            #endregion
+            //Als gebruiker wil ik een overzichtrapport kunnen opvragen voor een bepaalde provincie
+            
         }
     }
 }
